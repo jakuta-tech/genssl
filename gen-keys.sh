6ec008e7ba676441088778cbf33ba5e479dd402e
Florent Peterschmitt <florent@peterschmitt.fr>
2014-01-26 16:49:33 +0100

    document the code

--- gen-keys.sh
+++ gen-keys.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-keyname="default"
-if [ -z $1 ]; then
-    echo "give crt/key name"
-    exit 1
-else
+keyname=${keyname:-"default"}
+if [ ! -z $1 ]; then
     keyname=$1
 fi
 
@@ -16,5 +13,5 @@ fi
 
 echo "GENERATE $keyname"
 openssl req -new -keyform PEM -keyout "${keyname}.key" -outform PEM -out "${keyname}.csr" -nodes -newkey rsa:$size -subj "${subj}" ${extra_opts}
-echo "SIGNING"
+echo "SIGNING with ${ca_name}"
 openssl x509 -CA "${ca_name}.crt" -CAkey "${ca_name}.key" -CAserial serial -req -in "${keyname}.csr" -outform PEM -out "${keyname}.crt" -days $days
--- values.sample.sh
+++ values.sample.sh
@@ -1,33 +1,63 @@
 #!/bin/sh
 
+####
+##
+## If you want to override any of these parameters, simply export it before in
+## your shell, or copy `values.sample.sh` to `values.sh` and modify these values
+## before executing any script.
+##
+####
+
+##
+# ca_ prefixed vars are for Certificate Authority, other are for simple certs/keys.
+##
 export ca_size=${ca_size:-8192}
 export size=${size:-8192}
 
 export format=${format:-"PEM"}
 
+
+####
+##
+## Generate Certificate Authority's parameters.
+##
+####
+
+###
+# Used to build CA's subject.
+###
 ca_country_code=${ca_country_code:-"FR"}
 ca_state=${ca_state:-"France"}
 ca_locality=${ca_locality:-"Lille"}
 ca_org=${ca_org:-"Florent Peterschmitt"}
 ca_org_unit=${ca_org_unit:-"Florent Peterschmitt"}
 ca_common_name=${ca_common_name:-"Florent Peterschmitt"}
-ca_mail=${ca_mail:-"florent@peterschmitt.fr"}
+ca_email=${ca_email:-"florent@peterschmitt.fr"}
 
 export ca_extra_opts=${ca_extra_opts:-""}
 export ca_protect=${ca_protect:-"no"}
-export ca_name=${ca_name:-"FlorentPeterschmitt"}
+export ca_name=${ca_name:-"FlorentPeterschmitt"} # Will be used for certificate and key filename.
 export ca_days=${ca_days:-730}
-export ca_subj=${ca_subj:-"/C=${ca_country_code}/ST=${ca_state}/L=${ca_locality}/O=${ca_org}/OU=${ca_org_unit}/CN=${ca_common_name}/emailAddress=${ca_mail}"}
+export ca_subj=${ca_subj:-"/C=${ca_country_code}/ST=${ca_state}/L=${ca_locality}/O=${ca_org}/OU=${ca_org_unit}/CN=${ca_common_name}/emailAddress=${ca_email}"}
 
+####
+##
+## Generate certificate parameters.
+##
+####
+
+##
+# Used to build subject.
+##
 country_code=${country_code:-"FR"}
 state=${state:-"France"}
 locality=${locality:-"Lille"}
 org=${org:-"Florent Peterschmitt"}
 org_unit=${org_unit:-"Florent Peterschmitt"}
-common_name=${common_name:-"localhost"}
-mail=${mail:-"florent@peterschmitt.fr"}
+common_name=${common_name:-"localhost"} # FQDN for example.
+email=${email:-"florent@peterschmitt.fr"}
 
 export extra_opts=${extra_opts:-""}
 export protect=${protect:-"no"}
 export days=${days:-365}
-export subj=${subj:-"/C=${country_code}/ST=${state}/L=${locality}/O=${org}/OU=${org_unit}/CN=${common_name}/emailAddress=${mail}"}
+export subj=${subj:-"/C=${country_code}/ST=${state}/L=${locality}/O=${org}/OU=${org_unit}/CN=${common_name}/emailAddress=${email}"}
