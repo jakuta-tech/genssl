546e157685deb1de5ca17bd2365d6d685c3fcdd1
Florent Peterschmitt <florent@peterschmitt.fr>
2014-01-26 16:57:40 +0100

    set name in default values file

--- gen-keys.sh
+++ gen-keys.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-keyname=${keyname:-"default"}
-if [ ! -z $1 ]; then
-    keyname=$1
+if [ ! -z "${1}" ]; then
+    name="${1}"
 fi
 
 . ./values.sh || exit 1
@@ -11,7 +10,7 @@ if [ "$protect" = "no" ]; then
     extra_opts="${extra_opts} -nodes"
 fi
 
-echo "GENERATE $keyname"
-openssl req -new -keyform PEM -keyout "${keyname}.key" -outform PEM -out "${keyname}.csr" -nodes -newkey rsa:$size -subj "${subj}" ${extra_opts}
-echo "SIGNING with ${ca_name}"
-openssl x509 -CA "${ca_name}.crt" -CAkey "${ca_name}.key" -CAserial serial -req -in "${keyname}.csr" -outform PEM -out "${keyname}.crt" -days $days
+echo "GENERATE $name" 1>&2
+openssl req -new -keyform PEM -keyout "${name}.key" -outform PEM -out "${name}.csr" -nodes -newkey rsa:${size} -subj "${subj}" ${extra_opts}
+echo "SIGNING with ${ca_name}" 1>&2
+openssl x509 -CA "${ca_name}.crt" -CAkey "${ca_name}.key" -CAserial serial -req -in "${name}.csr" -outform PEM -out "${name}.crt" -days $days
--- values.sample.sh
+++ values.sample.sh
@@ -35,7 +35,7 @@ ca_common_name=${ca_common_name:-"Florent Peterschmitt"}
 ca_email=${ca_email:-"florent@peterschmitt.fr"}
 
 export ca_extra_opts=${ca_extra_opts:-""}
-export ca_protect=${ca_protect:-"no"}
+export ca_protect=${ca_protect:-"no"} # Shortcut for -nodes openssl option.
 export ca_name=${ca_name:-"FlorentPeterschmitt"} # Will be used for certificate and key filename.
 export ca_days=${ca_days:-730}
 export ca_subj=${ca_subj:-"/C=${ca_country_code}/ST=${ca_state}/L=${ca_locality}/O=${ca_org}/OU=${ca_org_unit}/CN=${ca_common_name}/emailAddress=${ca_email}"}
@@ -58,6 +58,7 @@ common_name=${common_name:-"localhost"} # FQDN for example.
 email=${email:-"florent@peterschmitt.fr"}
 
 export extra_opts=${extra_opts:-""}
-export protect=${protect:-"no"}
+export protect=${protect:-"no"} # Shortcut for -nodes openssl option.
+export name=${name:-"default-ssl"}
 export days=${days:-365}
 export subj=${subj:-"/C=${country_code}/ST=${state}/L=${locality}/O=${org}/OU=${org_unit}/CN=${common_name}/emailAddress=${email}"}
