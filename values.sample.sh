c356ed0c1a8de7d3ce93d476d01644fc68d2e04d
Florent Peterschmitt <florent@peterschmitt.fr>
2014-01-26 16:38:12 +0100

    moar default values

--- values.sample.sh
+++ values.sample.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export ca_size=8192
-export size=8192
+export ca_size=${ca_size:-8192}
+export size=${size:-8192}
 
-export format="PEM"
+export format=${format:-"PEM"}
 
 ca_country_code=${ca_country_code:-"FR"}
 ca_state=${ca_state:-"France"}
@@ -13,11 +13,11 @@ ca_org_unit=${ca_org_unit:-"Florent Peterschmitt"}
 ca_common_name=${ca_common_name:-"Florent Peterschmitt"}
 ca_mail=${ca_mail:-"florent@peterschmitt.fr"}
 
-export ca_extra_opts=""
-export ca_protect="no"
-export ca_name="FlorentPeterschmitt"
-export ca_days=730
-export ca_subj="/C=${ca_country_code}/ST=${ca_state}/L=${ca_locality}/O=${ca_org}/OU=${ca_org_unit}/CN=${ca_common_name}/emailAddress=${ca_mail}"
+export ca_extra_opts=${ca_extra_opts:-""}
+export ca_protect=${ca_protect:-"no"}
+export ca_name=${ca_name:-"FlorentPeterschmitt"}
+export ca_days=${ca_days:-730}
+export ca_subj=${ca_subj:-"/C=${ca_country_code}/ST=${ca_state}/L=${ca_locality}/O=${ca_org}/OU=${ca_org_unit}/CN=${ca_common_name}/emailAddress=${ca_mail}"}
 
 country_code=${country_code:-"FR"}
 state=${state:-"France"}
@@ -27,7 +27,7 @@ org_unit=${org_unit:-"Florent Peterschmitt"}
 common_name=${common_name:-"localhost"}
 mail=${mail:-"florent@peterschmitt.fr"}
 
-export extra_opts=""
-export protect="no"
-export days=365
-export subj="/C=${country_code}/ST=${state}/L=${locality}/O=${org}/OU=${org_unit}/CN=${common_name}/emailAddress=${mail}"
+export extra_opts=${extra_opts:-""}
+export protect=${protect:-"no"}
+export days=${days:-365}
+export subj=${subj:-"/C=${country_code}/ST=${state}/L=${locality}/O=${org}/OU=${org_unit}/CN=${common_name}/emailAddress=${mail}"}
