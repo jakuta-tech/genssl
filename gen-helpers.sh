c132d72e19b5136e15994481d209d4b994be326a
Florent Peterschmitt <florent@peterschmitt.fr>
2014-07-16 21:51:02 +0200

    gen_ident_certs

--- gen-helpers.sh
+++ gen-helpers.sh
@@ -7,3 +7,13 @@ gen_certs() {
         ./gen-keys.sh
     done
 }
+
+gen_ident_certs() {
+    for domain in ${domains}; do
+        export common_name="${domain}"
+        export name="${section}-${common_name}"
+        ./gen-keys.sh
+        . ./values.sh
+        openssl pkcs12 -export -out keys/${name}.p12 -inkey keys/${name}.key -in certs/${name}.crt -certfile ca/certs/${ca_name}.ca.crt
+    done
+}
